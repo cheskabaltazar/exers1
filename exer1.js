68f442a78d7381df28bf1e2c618de0bdc437657d
cheskabaltazar <f.cain.529262@umindanao.edu.ph>
2025-09-17 23:41:12 +0800

    Add files via upload

---
+++ exer1.js
@@ -0,0 +1,16 @@
+'use strict';
+const massMark=78;
+const heightMark=1.69;
+const massJohn=92;
+const heightJohn=1.95;
+
+const BMIMark=massMark/(heightMark * heightMark);
+const BMIJohn=massJohn/(heightJohn * heightJohn);
+const markHigherBMI=BMIMark>BMIJohn;
+console.log(BMIMark,BMIJohn,markHigherBMI);
+
+if(BMIMark>BMIJohn){
+    console.log("Mark's BMI ("+BMIMark+") is higher than John's BMI ("+BMIJohn+")");
+}else{
+    console.log("John's BMI ("+BMIJohn+") is higher than Mark's BMI ("+BMIMark+")");
+}
